682883d563822edb6b8af98612af8980a19bf990
Mohammed Abed <mbd411071@gmail.com>
2025-12-25 21:58:19 +0300

    Add Fonts to the project and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import TodoList from "./components/TodoList";
 import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Alexandria"],
+  },
+});
 
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        direction: "rtl",
-        backgroundColor: "#191B1E",
-      }}
-    >
-      <TodoList />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          direction: "rtl",
+          backgroundColor: "#191B1E",
+          fontFamily: "Alexandria",
+          fontWeight: "bold",
+        }}
+      >
+        <TodoList />
+      </div>
+    </ThemeProvider>
   );
 }
 
